93acd6e3daf5f4bcd761dac35e0438734091a45c
Ivan Leonardi <ivanthaisjesus@gmail.com>
2024-02-17 08:50:00 -0300

    add render

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://api-sgp.cyclic.app"
+    baseURL: "https://api-sgp-usf.onrender.com"
    // baseURL: "http://localhost:3333"
     //baseURL: "https://api-sgp-usf.onrender.com" 
 });
