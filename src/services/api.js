1390d16548af070f880c9d1cd28e526d8e09e599
Ivan Leonardi <ivanthaisjesus@gmail.com>
2024-03-06 00:54:59 -0300

    update url api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://api-sgp-usf.onrender.com"
-   // baseURL: "http://localhost:3333"
-    //baseURL: "https://api-sgp-usf.onrender.com" 
+    baseURL: "https://api-sgp-1.onrender.com"
+   // baseURL: "http://localhost:8000"     
 });
